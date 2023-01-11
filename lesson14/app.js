903e560261d0a726064be76ea65fe22aeb224281
nevainero <75412887+shaualievnurbolat@users.noreply.github.com>
2023-01-12 03:07:21 +0600

    Add files via upload

---
+++ lesson14/app.js
@@ -0,0 +1,130 @@
+//HW 
+
+// task 1
+
+    // const arr = []
+    // const reversedArr = []
+
+    // for (let i = 1; i <= 100; i++) {
+    // 	arr.push(i)
+    // }
+
+    // for (let i = arr.length - 1; i >= 0; i--) {
+    // 	reversedArr.push(arr[i])
+    // }
+
+    // console.log(reversedArr)
+
+// task 2 
+
+
+    // const arr = []
+
+    // for(let i = 1; i <= 99; i++) {
+    // 	if (i % 2) {
+    //   	arr.push(i)
+    //   }
+    // }
+
+    // console.log(arr)
+
+// task 3
+
+    // const arr = []
+
+    // for (let i = 2; i <= 20; i++) {
+    //     if(i % 2 == 0) {
+    //     arr.push(i)
+    // }
+    // }
+
+    // console.log(arr)
+
+// task 4 
+
+    // const arr = []
+    // let sum = 0
+
+    // for (let i = 2; i <= 20; i++) {
+    //     if (i % 2 == 0) {
+    //         arr.push(i)
+    //     }
+    // }
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum += arr[i]
+    // }
+
+    // console.log(sum)
+
+// task 5
+
+    // const arr = ['text', false, 12, undefined, '', 'nurbolat', 0, null, 34, 56, 'unsplash', 'github', 'pinterest']
+    // const newArr = []
+
+    // for (let i = 0; i < arr.length; i++) {
+    // 	if (!(arr[i] == undefined || arr[i] == false || arr[i] == '' || arr[i] == 0 || arr[i] == null)) {
+    //         newArr.push(arr[i])
+    //     }
+    // }
+
+    // console.log(`old version`, arr)
+    // console.log(`new version`, newArr)
+
+// task 6
+
+    // const numbers = [1, 54, 64, 23, 666, 23, 5, 7, 124, 76]
+    // let maxNumber = 0
+
+    // for (let i = 0; i <= numbers.length; i++) {
+    //     if (maxNumber < numbers[i]) {
+    //         maxNumber = numbers[i]
+    //     }
+    // }
+
+    // console.log(maxNumber)
+
+// task 7 
+
+    // const numbers = [1, 54, 64, 23, 666, 23, -45, 5, 7, 124, 76, -23]
+
+    // for (let i = 0; i <= numbers.length; i++) {
+    //     if (numbers[i] < numbers[0]) {
+    //         numbers[0] = numbers[i];
+    //     }
+    // }
+
+    // console.log(`min number: ${numbers[0]}`)
+
+// task 8
+
+// const firstArr = [1,2,3,4,5,6]
+// const secondArr = [1,4,2,3,4,5]
+
+// const textArr = ['book', 'laptop', 'name', 'think', 'code']
+// const secondTextArr = ['book', 'laptop', 'name', 'think', 'code']
+
+// const numbersArr = [23, 56, 4]
+// const secondNumbersArr = [23, 56, 4, 64]
+
+// function changeArr(firstArr, secondArr) {
+//     let bool;
+
+//     for (let i = 0; i <= firstArr.length; i++) {
+//         if (firstArr[i] == secondArr[i]) {
+//             bool = true
+//         } else {
+//             bool = false
+//             break
+//         }
+//     }
+
+//     console.log(`result: ${bool}`)
+// }
+
+// changeArr(firstArr, secondArr)
+// changeArr(textArr, secondTextArr)
+// changeArr(numbersArr, secondNumbersArr)
+
+
+
